59a55fcb694d2b0cd960d2c02ef1eb241f0ecb3e
stuferg <stuart.ferguson@kainos.com>
2023-06-29 12:08:28 +0100

    adding clientService and an exception

--- src/main/java/org/kainos/ea/api/ClientService.java
+++ src/main/java/org/kainos/ea/api/ClientService.java
@@ -1,9 +1,26 @@
 package org.kainos.ea.api;
 
+import org.kainos.ea.cli.ClientList;
+import org.kainos.ea.client.FailedToGetClientsException;
 import org.kainos.ea.db.ClientDao;
+
+import java.sql.SQLException;
+import java.util.List;
 
 public class ClientService {
 
     private ClientDao clientDao = new ClientDao();
+
+    public List<ClientList> getClientList() = throws FailedToGetClientsException {
+        try {
+            List<ClientList> clientList = clientDao.getClientsList();
+
+            return clientList;
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+
+            throw new FailedToGetClientsException;
+        }
+    }
 
 }
