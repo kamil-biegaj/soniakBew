99a25a6d8e08ff1f0aff47e5994d6e62a354ad54
stuferg <stuart.ferguson@kainos.com>
2023-06-29 11:49:49 +0100

    Adding start to clientDoa

---
+++ src/main/java/org/kainos/ea/db/ClientDao.java
@@ -0,0 +1,36 @@
+package org.kainos.ea.db;
+
+import org.kainos.ea.cli.Client;
+import org.kainos.ea.cli.ClientList;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ClientDao {
+
+    public List<ClientList> getClientsList() throws SQLException {
+        Connection c = dataBaseConnector.getConnection();
+
+        Statement st = c.createstatement();
+
+        ResultSet rs = st.excuteQuery(sdfkeabrf);
+
+        List<ClientList> clientList = new ArrayList<>();
+
+        while (rs.next()) {
+            ClientList client = new ClientList(
+                    rs.getString("Name"),
+                    rs.getString("Employee Name")
+                    rs.getString("Project Name")
+            );
+            clientList.add(client);
+        }
+
+        return clientList;
+    }
+
+}
