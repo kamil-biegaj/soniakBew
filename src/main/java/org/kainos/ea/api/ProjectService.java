8c2ff7cf236684ab8f6b92e94f8644967a135ac7
kamil-biegaj <137313537+kamil-biegaj@users.noreply.github.com>
2023-06-30 09:14:03 +0100

    Update ProjectService.java

--- src/main/java/org/kainos/ea/api/ProjectService.java
+++ src/main/java/org/kainos/ea/api/ProjectService.java
@@ -1,5 +1,18 @@
 package org.kainos.ea.api;
 
 public class ProjectService {
+  private ProjectDao projectDao = new ProjectDao();
 
+    public List<Project> getProjectList(){
+        try {
+            List<Project> projectLists;
+            projectLists = projectDao.getProjectsList();
+
+            return projectLists;
+        } catch (SQLException e) {
+            System.err.println(e.getMessage());
+        }
+
+        return null;
+    }
 }
